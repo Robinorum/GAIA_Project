0d69da0c583d381427f57b20f288d73a6bf102c9
Robinorum <118909581+Robinorum@users.noreply.github.com>
2024-11-14 22:56:16 +0100

    Update recherche.py

--- code/embeding/recherche.py
+++ code/embeding/recherche.py
@@ -11,19 +11,27 @@ preprocess = transforms.Compose([
 ])
 
 def get_embedding(image, model, device):
-    """
-    Transforme une image en embedding à l'aide du modèle ResNet18.
-    """
+    
     input_tensor = preprocess(image).unsqueeze(0).to(device)
     with torch.no_grad():
-        embedding = model(input_tensor)
+        embedding = model.encode_image(input_tensor)
+        embedding = embedding / embedding.norm(dim=-1, keepdim=True)
     return embedding.squeeze().cpu().numpy()
 
-def find_most_similar_image(image, index, titles, artists, model, device, k=1):
+def find_most_similar_image(image, index, titles, artists, model, device, k=1, threshold=0.90):
     """
     Trouve les images les plus similaires dans l'index FAISS.
+    Si la distance minimale dépasse le seuil, renvoie "Pas de correspondance".
     """
     input_embedding = get_embedding(image, model, device).astype("float32")
-    _, indices = index.search(np.array([input_embedding]), k)
+    
+    # Recherche dans l'index avec FAISS
+    distances, indices = index.search(np.array([input_embedding]), k)
+    
+    # Vérifier la distance minimale
+    if distances[0][0] < threshold:
+        return False
+    
+    # Retourner les informations de l'image la plus proche
     closest_image_info = [(titles[i], artists[i]) for i in indices[0]]
     return closest_image_info
