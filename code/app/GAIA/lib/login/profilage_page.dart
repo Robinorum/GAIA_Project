f4ebf75c9b7a9d3c0769d2d9b7ff750c2024f3a4
KaizenDev <118910216+DarkLyx@users.noreply.github.com>
2024-12-20 02:48:49 +0100

    new:brands_add

--- code/app/GAIA/lib/login/profilage_page.dart
+++ code/app/GAIA/lib/login/profilage_page.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/artwork_service.dart';
+import '../model/artwork.dart';
+import 'package:GAIA/provider/userProvider.dart';
 import 'package:GAIA/pages/home_page.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Profilage Page',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ProfilagePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
 
 class ProfilagePage extends StatefulWidget {
   const ProfilagePage({super.key});
@@ -26,56 +14,54 @@ class ProfilagePage extends StatefulWidget {
   State<ProfilagePage> createState() => _ProfilagePageState();
 }
 
-class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProviderStateMixin {
-  final List<Map<String, String>> artworks = [
-    {
-      'imageUrl': 'https://via.placeholder.com/600x800.png?text=Image+1',
-      'title': 'Oeuvre 1',
-      'location': 'Musée du Louvre',
-      'type': 'Cubisme',
-    },
-    {
-      'imageUrl': 'https://via.placeholder.com/600x800.png?text=Image+2',
-      'title': 'Oeuvre 2',
-      'location': 'Musée d\'Orsay',
-      'type': 'Impressionnisme',
-    },
-    {
-      'imageUrl': 'https://via.placeholder.com/600x800.png?text=Image+3',
-      'title': 'Oeuvre 3',
-      'location': 'Tate Modern',
-      'type': 'Suréalisme',
-    },
-    {
-      'imageUrl': 'https://via.placeholder.com/600x800.png?text=Image+4',
-      'title': 'Oeuvre 4',
-      'location': 'Guggenheim',
-      'type': 'Expressionnisme',
-    },
-    {
-      'imageUrl': 'https://via.placeholder.com/600x800.png?text=Image+5',
-      'title': 'Oeuvre 5',
-      'location': 'Centre Pompidou',
-      'type': 'Minimalisme',
-    },
-  ];
-
+class _ProfilagePageState extends State<ProfilagePage> {
+  late Future<List<Artwork>> _recommendedArtworks;
+  late FirebaseFirestore _firestore;
+  late FirebaseAuth _auth;
+  late User? _firebaseUser;
   int currentIndex = 0;
   double offset = 0;
   double angle = 0;
 
-  void handleSwipe(String direction) {
+  @override
+  void initState() {
+    super.initState();
+    _recommendedArtworks = ArtworkService().fetchArtworks(); // Récupération via l'API
+    _firestore = FirebaseFirestore.instance;
+    _auth = FirebaseAuth.instance;
+    _firebaseUser = _auth.currentUser;
+  }
+
+  Future<void> _addToBrands(String artworkId) async {
+    if (_firebaseUser != null) {
+      // Vérification de l'existence de l'utilisateur
+      final docSnapshot = await _firestore.collection('accounts').doc(_firebaseUser!.uid).get();
+      if (docSnapshot.exists) {
+        // Ajouter l'ID de l'œuvre au tableau 'brands' de l'utilisateur
+        await _firestore.collection('accounts').doc(_firebaseUser!.uid).update({
+          'brands': FieldValue.arrayUnion([artworkId]), // Ajouter l'ID de l'œuvre au tableau
+        });
+        // Log pour vérifier la valeur ajoutée
+        print("Ajouté à 'brands' : $artworkId");
+      }
+    }
+  }
+
+  void handleSwipe(String direction, String artworkId) {
     setState(() {
       if (direction == 'right') {
-        print('Liké: ${artworks[currentIndex]['title']}');
+        // Log pour vérifier le ID avant d'ajouter
+        print("Liké (id): $artworkId");
+        _addToBrands(artworkId); // Ajouter l'ID de l'œuvre au tableau 'brands'
       } else if (direction == 'left') {
-        print('Pas Liké: ${artworks[currentIndex]['title']}');
+        // Log pour vérifier l'ID du non-liker
+        print("Pas liké (id): $artworkId");
       }
       currentIndex++;
       offset = 0; // Réinitialisation de l'offset
-      angle = 0;  // Réinitialisation de l'angle
+      angle = 0; // Réinitialisation de l'angle
 
-      if (currentIndex == artworks.length) {
+      if (currentIndex == 5) {
         // Redirection vers MainPage une fois que toutes les cartes sont traitées
         Navigator.pushReplacement(
           context,
@@ -87,36 +73,61 @@ class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProvider
 
   @override
   Widget build(BuildContext context) {
+    // Récupérer l'utilisateur à partir du UserProvider
+    final user = Provider.of<UserProvider>(context).user;
+
     return Scaffold(
-      body: currentIndex < artworks.length
-          ? Center(
-              child: GestureDetector(
-                onPanUpdate: (details) {
-                  setState(() {
-                    offset += details.delta.dx; // Déplacer la carte
-                    angle = offset / 10; // Appliquer une rotation proportionnelle
-                  });
-                },
-                onPanEnd: (details) {
-                  if (offset > 100) {
-                    handleSwipe('right'); // Swipe droite
-                  } else if (offset < -100) {
-                    handleSwipe('left'); // Swipe gauche
-                  } else {
-                    setState(() {
-                      offset = 0; // Réinitialiser la position si le swipe est insuffisant
-                      angle = 0; // Réinitialiser la rotation
-                    });
-                  }
-                },
-                child: buildArtworkCard(artworks[currentIndex], offset, angle),
+      appBar: AppBar(
+        title: Text('Profilage de ${user?.username ?? "guest"}'), // Afficher le nom de l'utilisateur
+      ),
+      body: FutureBuilder<List<Artwork>>(
+        future: _recommendedArtworks,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                "Erreur lors du chargement: ${snapshot.error}",
+                style: const TextStyle(color: Colors.red),
               ),
-            )
-          : const Center(child: Text("Chargement...")),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("Aucune œuvre trouvée."));
+          }
+
+          final artworks = snapshot.data!;
+          return currentIndex < artworks.length
+              ? Center(
+                  child: GestureDetector(
+                    onPanUpdate: (details) {
+                      setState(() {
+                        offset += details.delta.dx; // Déplacer la carte
+                        angle = offset / 10; // Appliquer une rotation proportionnelle
+                      });
+                    },
+                    onPanEnd: (details) {
+                      if (offset > 100) {
+                        handleSwipe('right', artworks[currentIndex].id); // Swipe droite
+                      } else if (offset < -100) {
+                        handleSwipe('left', artworks[currentIndex].id); // Swipe gauche
+                      } else {
+                        setState(() {
+                          offset = 0; // Réinitialiser la position si le swipe est insuffisant
+                          angle = 0; // Réinitialiser la rotation
+                        });
+                      }
+                    },
+                    child: buildArtworkCard(artworks[currentIndex], offset, angle),
+                  ),
+                )
+              : const Center(child: Text("Chargement..."));
+        },
+      ),
     );
   }
 
-  Widget buildArtworkCard(Map<String, String> artwork, double offset, double angle) {
+  Widget buildArtworkCard(Artwork artwork, double offset, double angle) {
     return Transform.translate(
       offset: Offset(offset, 0), // Déplacer la carte horizontalement
       child: Transform.rotate(
@@ -142,7 +153,7 @@ class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProvider
                     ),
                   ],
                   image: DecorationImage(
-                    image: NetworkImage(artwork['imageUrl']!),
+                    image: artwork.toImage().image,
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -150,15 +161,11 @@ class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProvider
               const SizedBox(height: 20),
               // Informations sur l'œuvre
               Text(
-                artwork['title']!,
+                artwork.title,
                 style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
               Text(
-                'Localisation: ${artwork['location']}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              Text(
-                'Type: ${artwork['type']}',
+                'Type: ${artwork.movement}',
                 style: const TextStyle(
                     fontSize: 16, fontStyle: FontStyle.italic, color: Colors.grey),
               ),
@@ -168,12 +175,12 @@ class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProvider
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                    onPressed: () => handleSwipe('left'),
+                    onPressed: () => handleSwipe('left', artwork.id),
                     icon: const Icon(Icons.close, color: Colors.red, size: 40),
                   ),
                   const SizedBox(width: 50),
                   IconButton(
-                    onPressed: () => handleSwipe('right'),
+                    onPressed: () => handleSwipe('right', artwork.id),
                     icon: const Icon(Icons.favorite, color: Colors.green, size: 40),
                   ),
                 ],
@@ -185,5 +192,3 @@ class _ProfilagePageState extends State<ProfilagePage> with SingleTickerProvider
     );
   }
 }
-
-
