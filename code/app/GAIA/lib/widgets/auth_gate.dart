595480e48c452bf315581731f823c86d5c99fedd
Robinorum <robin.metais@isen.yncrea.fr>
2024-12-20 09:58:01 +0100

    authgate fixed with provider

--- code/app/GAIA/lib/widgets/auth_gate.dart
+++ code/app/GAIA/lib/widgets/auth_gate.dart
@@ -2,27 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:GAIA/pages/home_page.dart';
 import 'package:GAIA/login/login_page.dart';
+import 'package:provider/provider.dart';
+import 'package:GAIA/provider/userProvider.dart';
+import 'package:GAIA/model/appUser.dart'; // Modèle AppUser
 
 class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(child: Text('Something went wrong!')),
-          );
-        } else if (snapshot.hasData) {
-          return HomePage();
-        } else {
-          return const LoginPage(title: 'Login Page');
-        }
-      },
-    );
-  }
+  return StreamBuilder<User?>(
+    stream: FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        );
+      } else if (snapshot.hasError) {
+        return Scaffold(
+          body: Center(child: Text('Something went wrong!')),
+        );
+      } else if (snapshot.hasData) {
+        return FutureBuilder<AppUser>(
+          future: AppUser.fromAuth(snapshot.data!),
+          builder: (context, userSnapshot) {
+            if (userSnapshot.connectionState == ConnectionState.waiting) {
+              return Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            } else if (userSnapshot.hasError) {
+              return Scaffold(
+                body: Center(child: Text('Something went wrong!')),
+              );
+            } else if (userSnapshot.hasData) {
+              Provider.of<UserProvider>(context, listen: false).setUser(userSnapshot.data!);
+              return HomePage();
+            } else {
+              return const LoginPage(title: 'Login Page');
+            }
+          },
+        );
+      } else {
+        return const LoginPage(title: 'Login Page');
+      }
+    },
+  );
 }
+}
+  
